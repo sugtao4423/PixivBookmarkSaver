4955e9bd20026babfbeea3982846c8e0268ec3bf
sugtao4423 <sugtao4423@gmail.com>
2022-05-21 21:48:24 +0900

    クッキーとユーザーIDをコマンドライン引数から取れるように

--- PixivBookmarkSaver.php
+++ PixivBookmarkSaver.php
@@ -4,4 +4,13 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/Controllers/PixivBookmarkController.php';
 
+$opts = getopt('c:u:', ['cookie:', 'userId:']);
+$cookie = $opts['c'] ?? @$opts['cookie'];
+$userId = $opts['u'] ?? @$opts['userId'];
+
+if (!isset($cookie) || !isset($userId)) {
+    echo 'Usage: php PixivBookmarkSaver.php --cookie=<cookie> --userId=<userId>' . PHP_EOL;
+    exit(1);
+}
+
 (new PixivBookmarkController())->saveAllBookmarks($cookie, $userId);
